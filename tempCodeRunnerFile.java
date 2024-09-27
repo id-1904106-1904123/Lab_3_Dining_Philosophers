f3da4bc61a0a1c3e2fa664787ae8d49c1b8a6468
id-1904106-1904123 <u1904106@student.cuet.ac.bd>
2024-09-27 16:19:23 +0600

    Delete tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++
@@ -1,5 +0,0 @@
-
-            Table table = tables[tableIndex];
-            if (!table.getPhilosophers().isEmpty()) {
-                Philosopher philosopher = table.getPhilosophers().get(new Random().nextInt(table.getPhilosophers().size()));
-                System.out.println("Deadlock at table " + tableIndex + "! M
